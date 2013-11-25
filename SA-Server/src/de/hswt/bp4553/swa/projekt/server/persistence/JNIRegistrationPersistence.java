6885c7df3bb7bb6695154745fcf7fee30c0dff91
Michael Sieger <bp4553@bb-x-xterm.hswt.de>
2013-11-25 14:12:47 +0100

    Load library

--- SA-Server/src/de/hswt/bp4553/swa/projekt/server/persistence/JNIRegistrationPersistence.java
+++ SA-Server/src/de/hswt/bp4553/swa/projekt/server/persistence/JNIRegistrationPersistence.java
@@ -1,11 +1,16 @@
 package de.hswt.bp4553.swa.projekt.server.persistence;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 
 import de.hswt.bp4553.swa.projekt.model.Registration;
 
 public class JNIRegistrationPersistence implements RegistrationPersistence{
+	
+	static{
+		System.load(new File("libSA-Projekt.so").getAbsolutePath());
+	}
 
     @Override
     public native Collection<Registration> getAll() throws IOException;
