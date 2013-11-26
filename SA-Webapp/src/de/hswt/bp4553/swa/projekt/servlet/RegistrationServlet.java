f485f45c1dee95d14e5100be9a5a23a431137409
Michi <michael.sieger@student.hswt.de>
2013-11-26 22:39:38 +0100

    Servlet file upload

--- SA-Webapp/src/de/hswt/bp4553/swa/projekt/servlet/RegistrationServlet.java
+++ SA-Webapp/src/de/hswt/bp4553/swa/projekt/servlet/RegistrationServlet.java
@@ -1,22 +1,48 @@
 package de.hswt.bp4553.swa.projekt.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+
 public class RegistrationServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp)
 			throws ServletException, IOException {
-		PrintWriter writer = resp.getWriter();
+	    try {
+	        List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+	        for (FileItem item : items) {
+	            if (item.isFormField()) {
+	                // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
+	                String fieldname = item.getFieldName();
+	                String fieldvalue = item.getString();
+	                // ... (do your job here)
+	            } else {
+	                // Process form file field (input type="file").
+	                String fieldname = item.getFieldName();
+	                String filename = FilenameUtils.getName(item.getName());
+	                InputStream filecontent = item.getInputStream();
+	                // ... (do your job here)
+	            }
+	        }
+	    } catch (FileUploadException e) {
+	        throw new ServletException("Cannot parse multipart request.", e);
+	    }
 	}
 	
 
