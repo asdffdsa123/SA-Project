efddf8ebf31c50bbbe943cd88801966cf7957349
Michael Sieger <bp4553@bb-x-xterm.hswt.de>
2013-11-28 15:30:16 +0100

    Android javadoc

--- SA-Android/src/de/hswt/bp4553/swa/projekt/androidclient/MainActivity.java
+++ SA-Android/src/de/hswt/bp4553/swa/projekt/androidclient/MainActivity.java
@@ -20,6 +20,11 @@ import de.hswt.bp4553.swa.projekt.model.Gender;
 import de.hswt.bp4553.swa.projekt.model.Registration;
 import de.hswt.bp4553.swa.projekt.server.ServerConfig;
 
+/**
+ * Die GUI des Clients, mit welcher eine Registrierung eingegeben werden kann.
+ * @author bp4553
+ *
+ */
 public class MainActivity extends Activity {
 	
 	private static final DateFormat FORMAT = SimpleDateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMAN);
@@ -44,14 +49,12 @@ public class MainActivity extends Activity {
 		}
 		return new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strs);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 	
+	/**
+	 * Startet den Speichervorgang der Registierung.
+	 * Wird vom Speichern Button aufgerufen.
+	 * @param view
+	 */
 	public void save(View view){
 		try {
 			Registration reg = parseRegistration();
@@ -63,6 +66,10 @@ public class MainActivity extends Activity {
 
 	}
 	
+	/**
+	 * Zeigt Progressbar an oder versteckt sie.
+	 * @param b
+	 */
 	public void setBusy(boolean b){
 		ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
 		int v;
@@ -74,6 +81,11 @@ public class MainActivity extends Activity {
 		bar.setVisibility(v);
 	}
 	
+	/**
+	 * Erzeugt aus den eingegenen Daten ein Registration objekt.
+	 * @return
+	 * @throws ParseException
+	 */
 	private Registration parseRegistration() throws ParseException{
 		return new Registration(s(R.id.firstname), s(R.id.lastname), FORMAT.parse(s(R.id.birthday)), 
 								Fakulty.values()[i(R.id.faculty_spinner)], Gender.values()[i(R.id.gender_spinner)]);
--- SA-Android/src/de/hswt/bp4553/swa/projekt/androidclient/SaveTask.java
+++ SA-Android/src/de/hswt/bp4553/swa/projekt/androidclient/SaveTask.java
@@ -7,12 +7,23 @@ import android.widget.Toast;
 import de.hswt.bp4553.swa.projekt.client.socket.SocketClient;
 import de.hswt.bp4553.swa.projekt.model.Registration;
 
+/**
+ * Dieser asynchrone Task speichert eine Registrierung auf dem Server.
+ * @author bp4553
+ *
+ */
 public class SaveTask extends AsyncTask<Object, Object, Exception>{
 	
 	private final Registration reg;
 	private final MainActivity activity;
 	private final SocketClient client;
 	
+	/**
+	 * 
+	 * @param activity Die Activity auf der ein Ergebnis Popup angezeigt wird
+	 * @param reg Die Registrierung die abgepeichert werden soll
+	 * @param client Der Client der verwendet wird, um mit dem Server kontakt aufzunehmen
+	 */
 	public SaveTask(MainActivity activity, Registration reg, SocketClient client){
 		this.activity = activity;
 		this.reg = reg;
